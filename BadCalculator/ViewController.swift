9388dc9fe5f2c608673034ff20b580eeeff010c2
Grace Gong <gongyawengrace@gmail.com>
2015-05-28 12:06:10 -0700

    may.28.2015 12:05pm  calculator no clear button

--- BadCalculator/ViewController.swift
+++ BadCalculator/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var answerField: UILabel?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func buttonTapped(theButton: UIButton) {
+        if answerField!.text == "0" {
+          answerField!.text = theButton.titleLabel!.text
+        } else {
+          answerField!.text = answerField!.text! + theButton.titleLabel!.text!
+        }
+    }
 
+     @IBAction func plusTapped(theButton: UIButton) {
+        println("plus tapped")
+    }
+     @IBAction func minusTapped(theButton: UIButton) {
+        println("minus tapped")
+    }
 
 }
+    
+
+
+
 
